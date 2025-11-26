d954d6467dd577f3eeec9e8de273969bf75d91d5
oNya <168719319+oNya685@users.noreply.github.com>
2025-11-26 15:35:09 +0800

    Increase PPO mini batch size from 16 to 128
    128 = 16 * 8 (rollout.n)

--- examples/linearpo_trainer/linearpo_b1024n1_mb16_7b.sh
+++ examples/linearpo_trainer/linearpo_b1024n1_mb16_7b.sh
@@ -31,7 +31,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
